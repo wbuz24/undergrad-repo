74fc62d7e8705a95bba1e23d1568a160456fd41e
wbuz24 <wbuziak@vols.utk.edu>
2023-02-08 10:53:56 -0500

    02/08/2023

--- S23/cs202/checkerboard.cpp
+++ S23/cs202/checkerboard.cpp
@@ -15,19 +15,25 @@ int main() {
     //initialize
     int row, col, cycle_size, width;
     char start_char, curr_char;
-
     cin >> row >> col >> start_char >> cycle_size >> width;
     // read from cin, no output text yet
 
-    for (int i = 0; i < row; i++) { // row loop
-        for ( int r = 0; r < width; r++) { // reproduces rows 'width' # of times  
-            for (int j = 0; j < col; j++) { // column loop
-                for ( int k = 0; k < width; k++) { // reproduces char width times making a square matrix
-                    curr_char = start_char + (i + j) % cycle_size; // calculates current char
-                    cout << curr_char; // prints current character
+    if (row <= 0 || col <= 0 || width <= 0 || (start_char + cycle_size) > 127) {
+        return 0;
+    }
+
+    else {
+        for (int i = 0; i < row; i++) { // row loop
+            for ( int r = 0; r < width; r++) { // reproduces rows 'width' # of times  
+                for (int j = 0; j < col; j++) { // column loop
+                    for ( int k = 0; k < width; k++) { // reproduces char width times making a square matrix
+                        curr_char = start_char + (i + j) % cycle_size; // calculates current char
+                        cout << curr_char; // prints current character
+                    }
                 }
+                cout << endl;
             }
-            cout << endl;
         }
+        return 0;
     }
 }
