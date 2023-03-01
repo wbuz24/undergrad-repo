eb57aaeb77663be56836a15cb813dfcbdbcab341
wbuz24 <wbuziak@vols.utk.edu>
2023-03-01 09:58:13 -0500

    assembly notes

---
+++ S23/cs230/notes/mixedFloatPrac.cpp
@@ -0,0 +1,20 @@
+/* Class notes March 1, 2023
+ * Mixing float and double data types
+ * Similar to exam question
+ *
+*/
+
+#include <cstdio>
+
+extern "C" {
+  double myfunc(double *value, double value2); // (a0, fa0) storing a pointer and double 
+}
+
+int main() {
+  double a = 10;
+  double b = 20;
+
+  double result = myfunc(&a, b); // not actually passing a double, passing a pointer
+
+  printf("%lf\n", result);
+}
