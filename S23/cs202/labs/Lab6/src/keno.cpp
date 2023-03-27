6fde48dd370744304aae894a3a3db02abfdf8b69
wbuz24 <wbuziak@vols.utk.edu>
2023-03-27 17:23:55 -0400

    Lab 6 finished

--- S23/cs202/labs/Lab6/src/keno.cpp
+++ S23/cs202/labs/Lab6/src/keno.cpp
@@ -3,23 +3,29 @@
  * Lab 6
  * Keno.cpp
  * Use fraction.cpp to calculate the probability of winning money in the game of keno
-*/ 
+ */ 
 
 #include "fraction.hpp"
 #include <iostream>
 using namespace std;
 
 int main() {
-  int nballs, i;
-  double bet;
+    int nballs, cat;
+    double bet, res = 0, payout;
 
-  cin >> bet;
-  cin >> nballs;
-  
-  printf("Bet: %2f\nBalls Picked: %d\n", bet, nballs);
-  for (i = 3; i <= nballs; i++) {
+    cin >> bet;
+    cin >> nballs;
 
-    printf("Probability of catching %d of %d: %d -- Expected return: %f", i, nballs, bet);
-  }
-  
+    Fraction curr; // initiate the fraction class
+    printf("Bet: %.2f\nBalls Picked: %d\n", bet, nballs);
+    while (cin >> cat >> payout) { // read each pair from stdin  
+        curr.Multiply_Binom(80 - nballs, 20 - cat); // equation from write up
+        curr.Multiply_Binom(nballs, cat);
+        curr.Divide_Binom(80, 20);
+        cout << "  Probability of catching "<< cat <<" of " << nballs << ": "<< curr.Calculate_Product() << " -- Expected return: " << curr.Calculate_Product() * payout << endl;
+        res += curr.Calculate_Product() * payout; // collect each expected return
+        curr.Clear(); // clear for next iteration
+    }
+    printf("Your return per bet: %.2f\nNormalized: %.2f\n", res - bet, (res - bet) / bet);
+    return 0;
 }
