771c491ced8d622a762d2048f9547cb8f626f9c9
wbuz24 <wbuziak@vols.utk.edu>
2023-09-14 14:07:02 -0400

    folder cleanup

--- F23/cs302/labs/Lab3/cs302-midi.cpp
+++ F23/cs302/labs/Lab3/cs302-midi.cpp
@@ -16,10 +16,15 @@ void CS302_Midi::el_to_nd()
 
 void CS302_Midi::nd_to_el()
 {
-  map <int, multimap <double, Event *> > tmpmap; // temporary map
+  map <int, multimap <double, Event *> > tmpmap; // temporary map of events
+  multimap <double, ND*>::iterator mit;
+  map <int, multimap <double, ND *> >::iterator it;  
+  el = new EventList;
 
+  for (mit = CS302_Midi::nd->begin(); mit != CS302_Midi::nd->end(); mit++) {
+    // if (rint(mit->first) != rint(mit++->first)) mit++;
+  }
   // traverse the ND multi-map
   // access the "key" element of the ND class
   // create a new event and add it to the events list
-  el = new EventList;
 }
