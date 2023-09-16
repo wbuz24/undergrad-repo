cd6f00feda69533d2acf2854bb0b4cb963a1b41a
wbuz24 <wbuziak@vols.utk.edu>
2023-09-16 13:05:35 -0400

    lab 3

--- F23/cs302/labs/Lab3/cs302-midi.cpp
+++ F23/cs302/labs/Lab3/cs302-midi.cpp
@@ -1,3 +1,9 @@
+// Will Buziak
+// CS302 
+// Lab 3 
+// Midi Manipulation
+// write the methods nd_to_el & el_to_nd
+
 #include <iostream>
 #include <map>
 #include <vector>
@@ -17,14 +23,66 @@ void CS302_Midi::el_to_nd()
 void CS302_Midi::nd_to_el()
 {
   map <int, multimap <double, Event *> > tmpmap; // temporary map of events
-  multimap <double, ND*>::iterator mit;
-  map <int, multimap <double, ND *> >::iterator it;  
+  multimap <double, Event *> tmpmult; // temporary multimap
+  multimap <double, ND*>::iterator mit; // iterators
+  multimap <double, Event *>::iterator tmpmit;
+  map <int, multimap <double, Event *> >::iterator it;  
   el = new EventList;
+  int last_time = 0;
 
   for (mit = CS302_Midi::nd->begin(); mit != CS302_Midi::nd->end(); mit++) {
     // if (rint(mit->first) != rint(mit++->first)) mit++;
+    Event* no = new Event; // Damper Up/ OFF event
+    Event* ne = new Event; // create a new Damper Down/ ON event
+    if (mit->second->key == 'N') { // Note is found
+      no->key = 'F';
+      no->time = mit->second->stop;
+      no->v1 = mit->second->pitch;
+
+      ne->key = 'O'; // On
+      ne->time = mit->second->start; // time = time since last event
+      ne->v1 = mit->second->pitch; // v1 = pitch for O/F
+      ne->v2 = mit->second->volume;
+      
+    }
+    else { // 'D' for Damper
+      no->key = 'D'; // damper UP
+      no->time = mit->second->stop;
+      no->v1 = 0;
+
+      ne->key = 'D';
+      ne->time = mit->second->start;
+      ne->v1 = 1; // this denotes a damper DOWN
+      
+    }
+    tmpmult.insert(make_pair(mit->second->stop, no)); // insert into the multimap
+    tmpmult.insert(make_pair(mit->second->start, ne)); // now sorted by time
+
+    last_time = rint(mit->second->stop);
   }
-  // traverse the ND multi-map
+  // traverse the Event multi-map
+  for (tmpmit = tmpmult.begin(); tmpmit != tmpmult.end(); tmpmit++) {
+    // maps dont insert duplicates, therefore inserting the integer should remove competing events
+    if (tmpmit->second->key == 'F') { // OFF events
+      tmpmap.insert(make_pair(rint(tmpmit->first), tmpmult));
+    }
+    if (tmpmit->second->key == 'D' && tmpmit->second->v1 == 0) { // damper UP
+      tmpmap.insert(make_pair(rint(tmpmit->first), tmpmult));
+    }
+    if (tmpmit->second->key == 'D' && tmpmit->second->v1 == 1) { // damper DOWN
+      tmpmap.insert(make_pair(rint(tmpmit->first), tmpmult));
+    }
+    if (tmpmit->second->key == 'O') { // ON events
+      tmpmap.insert(make_pair(rint(tmpmit->first), tmpmult));
+    }
+  }
+
+  // traverse the temporary map and push all events onto el(), calculating the correct time.
+  for (it = tmpmap.begin(); it != tmpmap.end(); it++) {
+    for (tmpmit = it->second.begin(); tmpmit != it->second.end(); tmpmit++) { 
+      el->push_back(tmpmit->second);
+    }
+  }
   // access the "key" element of the ND class
   // create a new event and add it to the events list
 }
---
+++ S22/README.md
@@ -0,0 +1,13 @@
+# Spring 2022
+
+This semester was the second semester of my junior year in mechanical engineering. In this semester, I decided to start taking computer science classes on the side. I also acquired an internship at Shaw Industries for process engineering in the following summer. Additionally, I was accepted to begin researching with the EESCL upon returning the following fall semester.
+
+## Class Schedule
+
+```
+Computer Science 101: Introduction to Computer Science
+ENG 102: English II
+ME331: Thermodynamics
+ME463: Mechanical Vibration
+ME466: Machine Design II
+```
