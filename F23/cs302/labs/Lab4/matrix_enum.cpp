6fdafb8136a18d1236f5bc93875f16b2fd626224
wbuz24 <wbuziak@vols.utk.edu>
2023-09-28 18:31:41 -0400

    printing

--- F23/cs302/labs/Lab4/matrix_enum.cpp
+++ F23/cs302/labs/Lab4/matrix_enum.cpp
@@ -17,24 +17,30 @@ public:
   vector <int> Perm;          /* Permutation of 0 .. (W-1), for the 'X' elements. */
   vector <int> Non_X;         /* This is the row/col id of each of the non-X elements. */
   vector <int> E_ID;          /* This is the row/col id of the E elements */
-  vector <vector <int> > im;  // vector of vectors to map the permutations
+  vector <vector <char> > im;  // vector of vectors to map the permutations
   void Print();               /* Print the matrix defined by W, Perm and E_ID */
-  void Permute(int index, vector <vector <int> > im);    /* This is the recursive permuting method. */
-  void Choose(int index, vector <vector <int> > im);     /* This is the recursive n-choose-k method. */
+  void Permute(int index);    /* This is the recursive permuting method. */
+  void Choose(int index);     /* This is the recursive n-choose-k method. */
 };
 
 void Matrix::Print() {
+  size_t i, j;
+
+  for (i = 0; i < im.size(); i++) {
+    for (j = 0; j < im[i].size(); j++) {
+      printf("%c", im[i][j]);
+    }
+    printf("\n");
+  }
   return;
 }
 
-void Matrix::Permute (int index, vector <vector <int> > im) {
-  size_t i;
-  int tmp;
+void Matrix::Permute (int index) {
+  size_t i, j;
 
-  /* Base case -- we're done - print out the vector */
+  /* Base case -- we're done*/
 
-  if (index == Perm.size()) {
-    Print();
+  if (index == W) {
     return;  
   }
 
@@ -44,51 +50,58 @@ void Matrix::Permute (int index, vector <vector <int> > im) {
 
   for (i = index; i < Perm.size(); i++) {
 
-    tmp = Perm[i];         /* Swap Perm[index] with Perm[i] */
-    Perm[i] = Perm[index];
-    Perm[index] = tmp;
+    Perm[i] = 'X';
+    
+    Permute(index+1);
 
-    Permute(index+1, im);  
+    Perm[i] = '.';
 
-    tmp = Perm[i];          /* Swap back */
-    Perm[i] = Perm[index];
-    Perm[index] = tmp;
+ }
+
+  
+  for (i = 0; i < im.size(); i++) {
+    for (j = 0; j < im[i].size(); j++) {
+      if (im[i][j] != 'X') Non_X.push_back(i*W + j);
+    }
   }
 
-  Choose(index, im); // n choose k call
+
+  Choose(index); // n choose k call
 }
 
-void Matrix::Choose(int index, vector <vector <int> > im) {
-  size_t i;
+void Matrix::Choose(int index) {
 
   /* Base case -- if there are no more people to add, 
      print out the team and return */
 
-  if (index == E) return;
+  if (index == 0) {
+    Print();
+    return;
+  }
 
   /* This is a second base case -- if there are fewer people left to add
      than there are places left on the team, then it's impossible to finish,
      so simply return.  Ask yourself why this is better than testing whether
      index is equal to people.size(), and returning if so. */
 
-  if (W > Perm.size() - index) return;
+  if ((size_t)W > Perm.size() - index) return;
 
   /* Now, put the person in people[index] onto the team, and call GenTeams()
      recursively.  Afterwards, take the person off of the team. */
 
   Perm[index];
-  Choose(index+1, im);
+  Choose(index+1);
   Non_X.pop_back();
 
   /* Finally, call GenTeams() recursively without putting
      people[index] on the team. */
 
-  Choose(index+1, im);
+  Choose(index+1);
 }
 
 int main(int argc, char** argv) {
   Matrix* M = new Matrix;
-  size_t i, j;
+  int i;
 
 
   if (argc == 4) {
@@ -97,11 +110,10 @@ int main(int argc, char** argv) {
     M->P = atoi(argv[3]);
 
     M->im.resize(M->W);
-    for (i = 0; i < M->W; i++) M->im[i].resize(M->W);
-
-    M->Permute(0, M->im);
-  
-
+    M->Perm.resize(M->W, '.');
+    for (i = 0; i < M->W; i++) M->im[i].resize(M->W, '.');
+    
+    M->Permute(0);
   }
   return 0;
 }
