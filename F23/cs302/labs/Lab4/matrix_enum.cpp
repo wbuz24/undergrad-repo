a22e4fddd34f5e7bdcc68ec41fa5792c2d5045e9
wbuz24 <wbuziak@vols.utk.edu>
2023-09-29 14:53:48 -0400

    permutations are correct

--- F23/cs302/labs/Lab4/matrix_enum.cpp
+++ F23/cs302/labs/Lab4/matrix_enum.cpp
@@ -17,7 +17,7 @@ public:
   vector <int> Perm;          /* Permutation of 0 .. (W-1), for the 'X' elements. */
   vector <int> Non_X;         /* This is the row/col id of each of the non-X elements. */
   vector <int> E_ID;          /* This is the row/col id of the E elements */
-  vector <vector <char> > im;  // vector of vectors to map the permutations
+  vector <vector <int> > im;  // vector of vectors to map the permutations
   void Print();               /* Print the matrix defined by W, Perm and E_ID */
   void Permute(int index);    /* This is the recursive permuting method. */
   void Choose(int index);     /* This is the recursive n-choose-k method. */
@@ -37,64 +37,39 @@ void Matrix::Print() {
 
 void Matrix::Permute (int index) {
   size_t i, j;
+  Perm.clear(); // reset vector for permutations
+  Perm.resize(W, '.'); // resize vector with .'s
 
-  /* Base case -- we're done*/
-
-  if (index == W) {
+  if (index == W) { // base case, print
+    Print();
     return;  
   }
 
-  /* Otherwise, for each element of the vector, swap it with the element
-     in index, call Permute() recursively on the remainder of the
-     vector, and then swap it back. */
 
-  for (i = index; i < Perm.size(); i++) {
+  Perm[index] = 'X'; // insert X at the index
 
-    Perm[i] = 'X';
-    
-    Permute(index+1);
+  im.push_back(Perm); // push vector into 2D vector
+  Permute(index+1); // recursive call
+  Perm[index] = '.'; // replace
 
-    Perm[i] = '.';
-
- }
-
-  
   for (i = 0; i < im.size(); i++) {
     for (j = 0; j < im[i].size(); j++) {
       if (im[i][j] != 'X') Non_X.push_back(i*W + j);
     }
-  }
-
+  } 
 
   Choose(index); // n choose k call
 }
 
 void Matrix::Choose(int index) {
 
-  /* Base case -- if there are no more people to add, 
-     print out the team and return */
+  if (E == E_ID.size()) return; // there are no more E's to enumerate
 
-  if (index == 0) {
-    Print();
-    return;
-  }
+  if ((size_t)E > E_ID.size() - index) return; // second base case
 
-  /* This is a second base case -- if there are fewer people left to add
-     than there are places left on the team, then it's impossible to finish,
-     so simply return.  Ask yourself why this is better than testing whether
-     index is equal to people.size(), and returning if so. */
-
-  if ((size_t)W > Perm.size() - index) return;
-
-  /* Now, put the person in people[index] onto the team, and call GenTeams()
-     recursively.  Afterwards, take the person off of the team. */
-
-  Perm[index];
+  im[index][Non_X[index] % index*W] = 'E'; // im[index][j]
+  E_ID.push_back(Non_X[index]); // push index*w + j onto E_ID
   Choose(index+1);
-  Non_X.pop_back();
-
-  /* Finally, call GenTeams() recursively without putting
-     people[index] on the team. */
 
   Choose(index+1);
 }
@@ -104,16 +79,13 @@ int main(int argc, char** argv) {
   int i;
 
 
-  if (argc == 4) {
+  if (argc == 4) { // Gather all command line arguments
     M->W = atoi(argv[1]);
     M->E = atoi(argv[2]);
     M->P = atoi(argv[3]);
 
-    M->im.resize(M->W);
-    M->Perm.resize(M->W, '.');
-    for (i = 0; i < M->W; i++) M->im[i].resize(M->W, '.');
-    
-    M->Permute(0);
+    M->Permute(0); // permutation call
   }
+  delete M; // clear for memory
   return 0;
 }
