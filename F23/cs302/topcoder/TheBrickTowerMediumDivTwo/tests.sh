ce2f621b07bda210b38221809e9f95ad5a3d96f7
wbuz24 <wbuziak@vols.utk.edu>
2023-10-02 12:39:50 -0400

    topcoder

---
+++ F23/cs302/topcoder/TheBrickTowerMediumDivTwo/tests.sh
@@ -0,0 +1,359 @@
+./a.out 0
+./a.out 1
+./a.out 2
+./a.out 3
+echo 15 38 42 11 | ./a.out -
+echo 2 40 1 44 | ./a.out -
+echo 11 6 | ./a.out -
+echo 22 46 31 36 46 42 | ./a.out -
+echo 32 28 30 43 34 | ./a.out -
+echo 39 27 26 18 19 44 | ./a.out -
+echo 6 41 15 37 | ./a.out -
+echo 47 36 31 13 17 | ./a.out -
+echo 18 17 19 7 9 24 | ./a.out -
+echo 23 | ./a.out -
+echo 31 3 42 | ./a.out -
+echo 28 32 34 | ./a.out -
+echo 6 35 23 8 26 14 9 | ./a.out -
+echo 14 6 46 30 15 40 35 | ./a.out -
+echo 29 4 31 25 6 | ./a.out -
+echo 19 | ./a.out -
+echo 45 26 43 | ./a.out -
+echo 41 19 19 | ./a.out -
+echo 6 10 8 25 39 | ./a.out -
+echo 25 16 | ./a.out -
+echo 6 27 36 31 | ./a.out -
+echo 18 | ./a.out -
+echo 6 | ./a.out -
+echo 19 19 | ./a.out -
+echo 31 32 6 27 47 32 28 | ./a.out -
+echo 4 | ./a.out -
+echo 19 27 11 | ./a.out -
+echo 39 47 13 46 17 25 | ./a.out -
+echo 45 24 18 11 10 | ./a.out -
+echo 36 36 2 13 48 30 30 | ./a.out -
+echo 17 1 3 14 19 25 | ./a.out -
+echo 39 43 49 | ./a.out -
+echo 19 41 32 | ./a.out -
+echo 48 6 47 5 | ./a.out -
+echo 40 22 2 | ./a.out -
+echo 29 15 19 47 7 10 13 | ./a.out -
+echo 13 47 33 23 | ./a.out -
+echo 11 6 18 16 40 | ./a.out -
+echo 4 47 21 44 23 | ./a.out -
+echo 26 6 31 35 42 21 | ./a.out -
+echo 38 19 36 6 25 10 18 | ./a.out -
+echo 23 32 30 | ./a.out -
+echo 22 44 43 17 25 | ./a.out -
+echo 7 23 | ./a.out -
+echo 19 19 | ./a.out -
+echo 47 29 13 47 25 35 | ./a.out -
+echo 25 38 | ./a.out -
+echo 25 13 42 | ./a.out -
+echo 38 49 10 28 | ./a.out -
+echo 45 25 43 41 5 6 | ./a.out -
+echo 15 37 | ./a.out -
+echo 25 26 33 23 | ./a.out -
+echo 27 48 45 | ./a.out -
+echo 25 44 34 25 21 | ./a.out -
+echo 35 2 | ./a.out -
+echo 45 44 28 | ./a.out -
+echo 9 22 | ./a.out -
+echo 9 22 | ./a.out -
+echo 1 40 23 13 1 | ./a.out -
+echo 30 | ./a.out -
+echo 16 31 30 25 | ./a.out -
+echo 47 | ./a.out -
+echo 44 7 44 | ./a.out -
+echo 40 38 18 30 13 21 | ./a.out -
+echo 39 | ./a.out -
+echo 26 32 48 5 6 30 | ./a.out -
+echo 27 | ./a.out -
+echo 13 6 | ./a.out -
+echo 12 46 8 47 27 9 | ./a.out -
+echo 30 28 13 5 48 49 | ./a.out -
+echo 8 5 5 7 49 | ./a.out -
+echo 30 | ./a.out -
+echo 23 26 28 15 5 48 | ./a.out -
+echo 1 | ./a.out -
+echo 7 49 40 39 25 | ./a.out -
+echo 15 28 42 11 38 12 | ./a.out -
+echo 28 39 36 | ./a.out -
+echo 13 48 31 34 25 19 | ./a.out -
+echo 25 22 37 29 47 27 | ./a.out -
+echo 32 | ./a.out -
+echo 22 5 33 45 33 | ./a.out -
+echo 14 40 6 6 25 | ./a.out -
+echo 48 13 | ./a.out -
+echo 15 33 35 | ./a.out -
+echo 13 26 28 | ./a.out -
+echo 39 | ./a.out -
+echo 16 10 49 35 36 18 26 | ./a.out -
+echo 3 18 13 14 38 27 21 | ./a.out -
+echo 24 18 9 17 40 4 | ./a.out -
+echo 22 34 21 33 10 44 2 | ./a.out -
+echo 9 1 27 10 | ./a.out -
+echo 11 | ./a.out -
+echo 15 33 38 | ./a.out -
+echo 44 6 | ./a.out -
+echo 33 4 12 36 14 | ./a.out -
+echo 36 20 48 | ./a.out -
+echo 27 8 29 20 15 | ./a.out -
+echo 26 40 12 | ./a.out -
+echo 35 43 11 47 6 11 | ./a.out -
+echo 46 37 13 5 | ./a.out -
+echo 20 28 25 26 36 | ./a.out -
+echo 39 4 7 47 2 22 27 | ./a.out -
+echo 42 34 33 47 49 | ./a.out -
+echo 5 39 23 34 7 22 27 | ./a.out -
+echo 29 44 37 | ./a.out -
+echo 30 18 6 | ./a.out -
+echo 41 | ./a.out -
+echo 44 9 28 8 46 38 | ./a.out -
+echo 47 12 2 37 31 | ./a.out -
+echo 3 46 7 42 24 | ./a.out -
+echo 38 | ./a.out -
+echo 2 16 47 16 | ./a.out -
+echo 26 45 12 19 | ./a.out -
+echo 33 7 18 13 25 45 30 | ./a.out -
+echo 14 16 27 9 35 | ./a.out -
+echo 7 7 3 10 | ./a.out -
+echo 11 16 34 | ./a.out -
+echo 15 3 22 20 12 | ./a.out -
+echo 44 34 47 2 41 | ./a.out -
+echo 46 20 13 19 20 32 | ./a.out -
+echo 9 27 35 | ./a.out -
+echo 13 45 | ./a.out -
+echo 25 29 32 34 20 14 | ./a.out -
+echo 34 27 14 30 | ./a.out -
+echo 34 26 | ./a.out -
+echo 4 9 36 46 41 36 | ./a.out -
+echo 31 45 14 42 39 | ./a.out -
+echo 38 35 45 37 5 | ./a.out -
+echo 2 29 21 40 19 29 12 | ./a.out -
+echo 6 43 43 | ./a.out -
+echo 14 21 3 | ./a.out -
+echo 48 32 41 47 37 | ./a.out -
+echo 11 27 30 | ./a.out -
+echo 42 | ./a.out -
+echo 44 46 42 18 16 45 15 | ./a.out -
+echo 46 10 24 8 2 | ./a.out -
+echo 48 17 37 25 42 36 | ./a.out -
+echo 2 | ./a.out -
+echo 22 43 38 33 9 37 | ./a.out -
+echo 34 11 35 39 45 9 11 | ./a.out -
+echo 3 | ./a.out -
+echo 16 15 23 17 45 27 10 | ./a.out -
+echo 41 37 21 44 26 47 38 | ./a.out -
+echo 12 1 24 | ./a.out -
+echo 46 32 5 4 28 24 | ./a.out -
+echo 48 11 31 29 43 10 | ./a.out -
+echo 15 14 15 28 36 25 34 | ./a.out -
+echo 8 7 17 32 34 | ./a.out -
+echo 40 23 33 | ./a.out -
+echo 7 35 | ./a.out -
+echo 24 29 46 | ./a.out -
+echo 28 8 43 | ./a.out -
+echo 43 26 17 2 39 | ./a.out -
+echo 25 48 | ./a.out -
+echo 44 49 22 | ./a.out -
+echo 34 24 4 44 49 22 26 | ./a.out -
+echo 38 | ./a.out -
+echo 21 31 | ./a.out -
+echo 37 39 12 | ./a.out -
+echo 38 36 30 | ./a.out -
+echo 4 21 | ./a.out -
+echo 42 25 14 21 14 | ./a.out -
+echo 7 35 | ./a.out -
+echo 3 37 31 35 46 | ./a.out -
+echo 32 20 18 | ./a.out -
+echo 35 35 13 6 8 | ./a.out -
+echo 46 28 11 28 47 21 30 | ./a.out -
+echo 39 6 30 | ./a.out -
+echo 35 32 48 47 22 | ./a.out -
+echo 23 11 46 37 37 28 46 | ./a.out -
+echo 34 28 39 24 25 42 10 | ./a.out -
+echo 42 48 15 30 31 | ./a.out -
+echo 6 1 | ./a.out -
+echo 4 48 27 | ./a.out -
+echo 42 1 43 | ./a.out -
+echo 36 41 36 38 | ./a.out -
+echo 1 2 11 3 29 | ./a.out -
+echo 18 37 27 | ./a.out -
+echo 21 25 6 47 48 28 26 | ./a.out -
+echo 41 27 7 21 16 44 | ./a.out -
+echo 30 45 11 7 | ./a.out -
+echo 47 38 27 17 24 48 23 | ./a.out -
+echo 35 | ./a.out -
+echo 19 15 7 20 42 29 | ./a.out -
+echo 25 16 5 23 | ./a.out -
+echo 42 35 38 | ./a.out -
+echo 43 2 33 32 | ./a.out -
+echo 49 36 37 | ./a.out -
+echo 29 12 17 26 23 6 10 | ./a.out -
+echo 14 31 24 9 48 | ./a.out -
+echo 46 | ./a.out -
+echo 3 37 12 | ./a.out -
+echo 37 47 42 3 9 | ./a.out -
+echo 3 13 34 20 | ./a.out -
+echo 5 18 35 27 30 | ./a.out -
+echo 2 34 21 21 | ./a.out -
+echo 36 34 38 43 27 34 | ./a.out -
+echo 18 44 23 15 | ./a.out -
+echo 45 38 32 | ./a.out -
+echo 18 42 36 35 4 | ./a.out -
+echo 29 2 36 11 5 31 | ./a.out -
+echo 14 27 6 10 15 45 37 | ./a.out -
+echo 22 46 5 | ./a.out -
+echo 9 29 4 12 | ./a.out -
+echo 18 | ./a.out -
+echo 30 24 | ./a.out -
+echo 41 2 43 37 43 12 25 | ./a.out -
+echo 1 30 31 29 8 16 | ./a.out -
+echo 9 46 1 7 27 19 | ./a.out -
+echo 39 30 41 33 23 13 11 | ./a.out -
+echo 41 40 17 17 31 27 | ./a.out -
+echo 41 9 9 | ./a.out -
+echo 31 | ./a.out -
+echo 5 | ./a.out -
+echo 2 | ./a.out -
+echo 15 45 14 36 6 31 | ./a.out -
+echo 44 33 9 7 17 35 30 | ./a.out -
+echo 14 29 3 32 30 | ./a.out -
+echo 16 41 9 | ./a.out -
+echo 10 46 | ./a.out -
+echo 27 | ./a.out -
+echo 6 25 4 | ./a.out -
+echo 25 14 | ./a.out -
+echo 26 25 7 14 26 11 | ./a.out -
+echo 6 31 35 6 20 44 10 | ./a.out -
+echo 28 11 3 15 12 44 | ./a.out -
+echo 34 32 35 39 8 | ./a.out -
+echo 33 36 17 9 28 46 | ./a.out -
+echo 7 28 45 16 | ./a.out -
+echo 12 30 2 | ./a.out -
+echo 3 30 17 8 42 1 | ./a.out -
+echo 31 10 | ./a.out -
+echo 24 18 | ./a.out -
+echo 49 44 24 47 39 10 11 | ./a.out -
+echo 9 15 | ./a.out -
+echo 22 | ./a.out -
+echo 3 27 18 40 36 37 | ./a.out -
+echo 49 12 34 44 15 | ./a.out -
+echo 31 48 46 24 15 11 | ./a.out -
+echo 43 47 14 | ./a.out -
+echo 2 41 22 47 20 | ./a.out -
+echo 44 37 48 24 | ./a.out -
+echo 30 34 33 7 | ./a.out -
+echo 42 16 3 37 8 | ./a.out -
+echo 5 9 | ./a.out -
+echo 16 15 43 32 34 | ./a.out -
+echo 26 36 2 39 3 1 | ./a.out -
+echo 49 31 32 30 13 | ./a.out -
+echo 2 42 31 49 | ./a.out -
+echo 24 34 18 36 38 10 | ./a.out -
+echo 1 19 3 41 9 30 3 | ./a.out -
+echo 5 35 31 30 18 49 45 | ./a.out -
+echo 14 | ./a.out -
+echo 36 2 30 | ./a.out -
+echo 18 48 | ./a.out -
+echo 35 16 49 9 22 16 4 | ./a.out -
+echo 46 33 5 26 46 30 9 | ./a.out -
+echo 7 20 21 11 29 39 15 | ./a.out -
+echo 44 36 19 2 5 36 36 | ./a.out -
+echo 4 36 36 27 | ./a.out -
+echo 40 1 11 10 31 25 44 | ./a.out -
+echo 39 40 35 | ./a.out -
+echo 11 46 13 14 3 | ./a.out -
+echo 29 42 | ./a.out -
+echo 14 10 25 4 | ./a.out -
+echo 41 16 20 35 16 9 41 | ./a.out -
+echo 35 17 17 | ./a.out -
+echo 2 11 3 36 | ./a.out -
+echo 37 7 43 39 15 11 | ./a.out -
+echo 44 29 38 5 43 5 1 | ./a.out -
+echo 46 44 | ./a.out -
+echo 33 | ./a.out -
+echo 35 44 46 33 5 | ./a.out -
+echo 17 25 32 22 2 15 23 | ./a.out -
+echo 2 20 | ./a.out -
+echo 9 11 30 | ./a.out -
+echo 47 | ./a.out -
+echo 8 | ./a.out -
+echo 11 46 27 4 33 | ./a.out -
+echo 39 30 32 | ./a.out -
+echo 36 29 | ./a.out -
+echo 14 | ./a.out -
+echo 12 47 21 | ./a.out -
+echo 25 7 37 8 8 | ./a.out -
+echo 16 | ./a.out -
+echo 17 9 | ./a.out -
+echo 6 39 33 41 48 24 | ./a.out -
+echo 9 20 8 31 | ./a.out -
+echo 15 24 | ./a.out -
+echo 28 39 | ./a.out -
+echo 29 23 24 29 34 | ./a.out -
+echo 40 17 33 31 18 | ./a.out -
+echo 32 48 25 35 28 48 37 | ./a.out -
+echo 41 22 | ./a.out -
+echo 46 11 17 | ./a.out -
+echo 45 48 | ./a.out -
+echo 25 21 44 44 4 27 | ./a.out -
+echo 8 32 43 7 8 26 | ./a.out -
+echo 4 44 27 25 49 | ./a.out -
+echo 42 9 23 33 21 | ./a.out -
+echo 15 39 4 49 46 | ./a.out -
+echo 40 15 48 40 21 | ./a.out -
+echo 27 | ./a.out -
+echo 13 17 8 10 | ./a.out -
+echo 24 22 49 | ./a.out -
+echo 20 36 6 11 2 26 43 | ./a.out -
+echo 2 | ./a.out -
+echo 14 10 | ./a.out -
+echo 25 47 | ./a.out -
+echo 47 44 6 24 10 1 | ./a.out -
+echo 19 18 21 36 | ./a.out -
+echo 29 22 24 30 26 24 17 | ./a.out -
+echo 11 35 | ./a.out -
+echo 19 17 10 45 18 36 22 | ./a.out -
+echo 30 16 29 13 22 | ./a.out -
+echo 10 | ./a.out -
+echo 24 44 20 27 22 41 46 | ./a.out -
+echo 23 | ./a.out -
+echo 22 | ./a.out -
+echo 27 36 30 | ./a.out -
+echo 28 8 26 37 | ./a.out -
+echo 12 34 | ./a.out -
+echo 42 | ./a.out -
+echo 2 34 14 25 11 | ./a.out -
+echo 13 32 16 12 18 49 3 | ./a.out -
+echo 40 37 19 6 | ./a.out -
+echo 1 25 10 26 33 | ./a.out -
+echo 35 45 1 49 37 46 | ./a.out -
+echo 7 39 | ./a.out -
+echo 1 | ./a.out -
+echo 4 21 10 23 1 32 42 | ./a.out -
+echo 17 35 10 40 49 36 35 | ./a.out -
+echo 19 | ./a.out -
+echo 41 26 | ./a.out -
+echo 33 38 33 16 39 30 49 | ./a.out -
+echo 32 46 9 | ./a.out -
+echo 20 27 44 7 8 23 | ./a.out -
+echo 9 5 28 1 48 | ./a.out -
+echo 30 4 | ./a.out -
+echo 31 | ./a.out -
+echo 44 46 10 23 43 | ./a.out -
+echo 42 32 27 | ./a.out -
+echo 7 | ./a.out -
+echo 49 30 42 41 11 6 | ./a.out -
+echo 4 16 48 | ./a.out -
+echo 47 12 3 28 47 | ./a.out -
+echo 18 43 38 33 | ./a.out -
+echo 1 | ./a.out -
+echo 23 49 47 24 34 21 | ./a.out -
+echo 23 | ./a.out -
+echo 28 | ./a.out -
+echo 41 44 | ./a.out -
+echo 6 12 | ./a.out -
+echo 14 1 | ./a.out -
+echo 26 10 43 3 34 3 11 | ./a.out -
