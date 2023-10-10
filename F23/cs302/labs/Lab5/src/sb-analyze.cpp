324a77f8ddf729fbe31691183dc0283bbdb489e6
wbuz24 <wbuziak@vols.utk.edu>
2023-10-10 17:42:26 -0400

    analyze

--- F23/cs302/labs/Lab5/src/sb-analyze.cpp
+++ F23/cs302/labs/Lab5/src/sb-analyze.cpp
@@ -1,3 +1,7 @@
+// Will Buziak
+// Lab 5
+// Analyze
+// Must analyze the superball board and print out scoring sets
 #include "disjoint_set.hpp"
 #include <iostream>
 #include <map>
@@ -82,7 +86,7 @@ Superball::Superball(int argc, char **argv)
 void Superball::analyze_superball(Disjoint_Set *d) {
   int i;
 
-  for (i = 0; i < r*c; i++) if (board[i] != '*') search(i, d);
+  for (i = 0; i < r*c; i++) if (board[i] != '*') search(i, d); // we know that the given square is not a '*'
 
   return;
 }
@@ -93,13 +97,14 @@ void Superball::search(int index, Disjoint_Set *d) {
   i = index / c;
   j = index % c;
   
+  // must check bounds, if the adjacent square is the same color & if it is already a part of the same set.
   if (i >= 1 && board[index] == board[(i-1)*c + j] && d->Find(index) != d->Find((i-1)*c + j)) d->Union(d->Find(index), d->Find((i-1)*c + j));
 
   if (i < r - 1 && board[index] == board[(i+1)*c + j] && d->Find(index) != d->Find((i+1)*c + j)) d->Union(d->Find(index), d->Find((i+1)*c + j));
 
   if (j >= 1 && board[index] == board[i*c + j - 1] && d->Find(index) != d->Find(i*c + j - 1)) d->Union(d->Find(index), d->Find(i*c + j - 1));
 
-  if (j < r - 1 && board[index] == board[i*c + j + 1] && d->Find(index) != d->Find(i*c + j + 1)) d->Union(d->Find(index), d->Find(i*c + j + 1));
+  if (j < c - 1 && board[index] == board[i*c + j + 1] && d->Find(index) != d->Find(i*c + j + 1)) d->Union(d->Find(index), d->Find(i*c + j + 1));
   return;
 }
 void Superball::store(Disjoint_Set *d) {
@@ -107,19 +112,21 @@ void Superball::store(Disjoint_Set *d) {
   const vector <int> *sizes;
 
   sizes = d->Get_Sizes();
-  for (i = 0; i < r*c; i++) {
-    if (sizes->at(d->Find(i)) > 1 && goals[i]) idmap.insert(make_pair(d->Find(i), i));
+  for (i = 0; i < r*c; i++) { // only insert into the map when it is a scoring set with a size greater than the minimum scoring size.
+    if (sizes->at(d->Find(i)) >= mss && goals[i] && board[i] != '*') idmap.insert(make_pair(d->Find(i), i));
   }
 
   return;
 }
 void Superball::print(Disjoint_Set *d) { 
-  map <int, int>::iterator mit;
+  map <int, int>::iterator mit; // map is keyed on set id and value is the index
   const vector <int> *sizes;
 
   sizes = d->Get_Sizes();
-  for (mit = idmap.begin(); mit != idmap.end(); mit++) {
-    printf("Size: %d Char: %c Scoring Cell: %d,%d\n", sizes->at(mit->first), board[mit->second], mit->second / c, mit->second % c);
+
+  printf("Scoring sets:\n");
+  for (mit = idmap.begin(); mit != idmap.end(); mit++) { // parse the map
+    printf("  Size: %2d  Char: %c  Scoring Cell: %d,%d\n", sizes->at(mit->first), board[mit->second], mit->second / c, mit->second % c);
   }
   return;
 }
@@ -129,10 +136,10 @@ int main(int argc, char** argv) {
   Disjoint_Set *d = new Disjoint_Set;
   Superball *s = new Superball(argc, argv);
   
-  d->Initialize(s->r * s->c);
+  d->Initialize(s->r * s->c); // initialize the disjoint set
   s->analyze_superball(d);
 
-  s->store(d);
+  s->store(d); // must wait until after creating the sets to store
   s->print(d);
   delete d;
   return 0;
