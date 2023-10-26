a5aee72a1e16596b27b48f3359bc15f3570e930a
wbuz24 <wbuziak@vols.utk.edu>
2023-10-26 01:46:13 -0400

    Lab 7 84/100

--- F23/cs302/labs/Lab7/maze_solve.cpp
+++ F23/cs302/labs/Lab7/maze_solve.cpp
@@ -16,7 +16,7 @@ class Node {
   public:
     int id; // index of the node
     list <int> edges; // all edges connected to the node
-    bool visited;
+    int visited;
 };
 
 class Graph {
@@ -24,12 +24,20 @@ class Graph {
     int row;
     int col;
     vector <Node *> nodes; // graph of nodes
-    void print() const;
+    void print(int index) const;
     void maze_solve(int index); // index and component number
 };
 
-void Graph::print() const {
-
+void Graph::print(int index) const {
+  
+  list <int>::iterator lit;
+  if (nodes[index]->visited < 1) return; // not part of the right path
+ 
+  printf("PATH %d\n", index);
+  nodes[index]->visited = 0; // I have now printed myself and can remove from my path
+  for (lit = nodes[index]->edges.begin(); lit != nodes[index]->edges.end(); lit++) {
+    if (nodes[*lit]->visited > 0) print(*lit); // only make a recursive call if this edge is a part of my path
+  }
   return;
 }
 
@@ -37,18 +45,27 @@ void Graph::maze_solve(int index) { // perform DFS to find the end
 
   list <int>::iterator lit;
   // base case 1: check the visited field
-  if (nodes[index]->visited) return;
-
-  // base case 2: I am the last cell
-  if (index == row*col - 1)
+  if (nodes[index]->visited >= 0) {
+    return;
+  }
 
   // set my visited field to true
-  nodes[index]->visited = true;
+  // visited field of 1 means I am a part of my path.
+  nodes[index]->visited = 1;
+
+  // base case 2: I am the last cell
+  if (index == row*col - 1) {
+    print(0); // recursively print the path
+    return;
+  }
+
   // call maze_solve on all of my edges
   // I should only be connected to my adjacent neighbors without walls
   for (lit = nodes[index]->edges.begin(); lit != nodes[index]->edges.end(); lit++) {
-    maze_solve(*lit);
+    maze_solve(*lit); // recursively search each of my edges
   }
+
+  nodes[index]->visited = 0; // if I return from all of my edges, I must not be a part of my path.
   
   return;
 }
@@ -67,17 +84,17 @@ int main() {
   for (i = 0; i < g->row*g->col; i++) {
     n = new Node;
     n->id = i;
-    n->visited = -1;
+    n->visited = -1; // -1 is not visited
 
     g->nodes.push_back(n);
   }
 
-  for (i = 0; i < g->nodes.size(); i++) {
+  for (i = 0; i < g->nodes.size(); i++) { // each node is connected to each surrounding node
     
-    if (i > 0) g->nodes[i]->edges.push_back(i-1);
+    if (i < g->row*g->col - g->col) g->nodes[i]->edges.push_back(i + g->col);
+    if (i < g->row*g->col - 1 && i + 1 % g->col > 0) g->nodes[i]->edges.push_back(i + 1);
+    if (i > 0 && i % g->col > 0) g->nodes[i]->edges.push_back(i-1);
     if (i > g->col - 1) g->nodes[i]->edges.push_back(i-g->col);
-    if (i < g->row*g->col - 1) g->nodes[i]->edges.push_back(i + 1);
-    if (i < g->row*g->col - g->col - 1) g->nodes[i]->edges.push_back(i + g->col);
   }
 
   while (cin >> word >> r >> c) { // insert walls inbetween each index, these nodes are no longer connected
