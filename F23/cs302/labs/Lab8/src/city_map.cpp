012b78e13fcb027f2ef9d1ab8f0b40e842475b5b
wbuz24 <wbuziak@vols.utk.edu>
2023-11-07 18:20:24 -0500

    Lab 8

--- F23/cs302/labs/Lab8/src/city_map.cpp
+++ F23/cs302/labs/Lab8/src/city_map.cpp
@@ -1,178 +1,161 @@
 // Will Buziak
-// CS302
+// Djikstras algorithm
 // Lab 8
-// Dijkstra's algorithm
-// Find the shortest path between two intersections by streets and avenues
+// CS302
 
 #include <iostream>
-#include <fstream>
-#include <cstdio>
-#include <cmath>
+#include "city_map.hpp"
 #include <math.h>
 #include <vector>
-#include "city_map.hpp"
 using namespace std;
 
-City_Map::City_Map() {
-  // build a graph with the following links format
-  
-  // streets are x and avenues are y coordinates
+double City_Map::Dijkstra(int avg_best_worst){
+  list <Road_Segment *>::iterator rit;
+  double time = 0;
+  double a = 0, s = 0;
+  vector < vector < double > > visited;
+  Intersection *curr;
+  Intersection *tmp;
+  double d = 0;
 
-  int strt, ave, highest, hstrt;
-  double x, y, gs, ga;
-  Intersection *i;
-  Road_Segment *r;
-  list <Intersection*>::iterator lit;
-  vector < vector < Intersection* > > tmp; // temporary adjacency matrix
-  size_t j, k;
-    
-  highest = 0;
-  hstrt = 0;
-  while (cin >> strt >> ave >> x >> y >> gs >> ga) { // read in the intersections
-    // create a new intersection
-    i = new Intersection; 
+  // insert first node
+  visited.resize(last->avenue+1);
+  tmp = last;
+  bfsq.insert(make_pair(0, first));
 
-    i->street = strt;
-    i->avenue = ave;
-    i->x = x;
-    i->y = y;
-    i->green[0] = gs;
-    i->green[1] = ga;
+  for (int i = 0; i < (int)visited.size(); i ++) visited[i].resize(last->street+1,-1);
 
-    if (ave > highest) highest = ave; // find highest avenue
-    if (strt > hstrt) hstrt = strt; // find highest street
-
-    if (strt == 0 && ave == 0) first = i; // set the first node
-    if (strt == hstrt && ave == highest) last = i; // set the last node
-    all.push_back(i);
-  }
-
-  // resize the temporary vector to the proper sizes (must add one to the highest indices)
-  tmp.resize(hstrt + 1);
-  for (j = 0; j < tmp.size(); j++) tmp[j].resize(highest + 1);  
-  
-  for (lit = all.begin(); lit != all.end(); lit++) {
-    // store into temporary vector
-    tmp[(*lit)->street][(*lit)->avenue] = *lit;
-  }
-
-  for (j = 0; j < tmp.size(); j++) {
-    for (k = 0; k < tmp[0].size(); k++) {
-      r = new Road_Segment;
-      // streets ascend from east to west (connect avenues)
-
-      // if the street or avenue is a multiple of 5, they go in both directions
-      if (tmp[j][k]->street % 5 == 0) { // check j & k bounds for each case
-        if (k < tmp[0].size() - 1) {
-          // add street to right (incrementing)
-          r->type = STREET;
-          r->number = j;
-          r->distance = sqrt(pow(abs(tmp[j][k+1]->x - tmp[j][k]->x), 2) + pow(abs(tmp[j][k+1]->y - tmp[j][k]->y), 2));
-          r->from = tmp[j][k];
-          r->to = tmp[j][k+1];
-
-          tmp[j][k]->adj.push_back(r);
-        } 
-        if (k > 0) {
-          // add street to the left (decrementing)
-          r->type = STREET;
-          r->number = j;
-          r->distance = sqrt(pow(abs(tmp[j][k]->x - tmp[j][k-1]->x), 2) + pow(abs(tmp[j][k]->y - tmp[j][k-1]->y), 2));
-
-          r->from = tmp[j][k];
-          r->to = tmp[j][k-1];
-
-          tmp[j][k]->adj.push_back(r);
-        }
+  visited[0][0] = 0;
+  // bfs
+  while (!bfsq.empty()) {
+    curr = bfsq.begin()->second;
+    curr -> bfsq_ptr = bfsq.begin();
+    for ( rit = curr->adj.begin(); rit!= curr->adj.end(); rit++){
+      // getting correcting time with respect to average, worst, and best case
+      switch (avg_best_worst){
+        case 'A':
+          switch ((*rit)->type){
+            case AVENUE: a = (*rit)->to->green[STREET];
+              s = (*rit)->to->green[AVENUE];time = (a*a) / (2 * (a + s)); break;
+            case STREET: a = (*rit)->to->green[AVENUE];
+              s = (*rit)->to->green[STREET];time = (a*a) / (2 * (a + s)); break;
+          } break;
+        case 'W':
+          switch((*rit)->type){
+            case STREET: time = (*rit)->to->green[AVENUE];
+              break;
+            case AVENUE: time = (*rit)->to->green[STREET];
+              break;
+          }
       }
-      else if (tmp[j][k]->street % 2 == 0) { // even
-        if (k < tmp[0].size() - 1) {
-          // one way street east to west (incrementing)
-          r->type = STREET;
-          r->number = j;
-          r->distance = sqrt(pow(abs(tmp[j][k+1]->x - tmp[j][k]->x), 2) + pow(abs(tmp[j][k+1]->y - tmp[j][k]->y), 2));
-          r->from = tmp[j][k];
-          r->to = tmp[j][k+1];
-
-          tmp[j][k]->adj.push_back(r);
-
+      // calculate the time from provided equation
+      double d = (*rit) ->distance / 30 * 3600 + (*rit)->from->best_time + time;
+      // if not visited or you can find a favorable time
+      if (visited[(*rit)->to->avenue][(*rit)->to->street] == -1 || d < visited[(*rit)->to->avenue][(*rit)->to->street]){
+        if ((*rit)->to->bfsq_ptr != bfsq.end()){
+           bfsq.erase((*rit)->to->bfsq_ptr);
         }
+        // set the visited field and nodes backedges
+        visited[(*rit)->to->avenue][(*rit)->to->street] = d;
+        (*rit)->to->backedge = (*rit);
+        (*rit)->to->best_time = d;
+        (*rit)->to->bfsq_ptr = bfsq.insert(make_pair(d, (*rit)->to));
       }
-      else { // odd is left to right (decrementing)
-        if (k > 0) {
-          // one way street west to east
-          r->type = STREET;
-          r->number = j;
-          r->distance = sqrt(pow(abs(tmp[j][k]->x - tmp[j][k-1]->x), 2) + pow(abs(tmp[j][k]->y - tmp[j][k-1]->y), 2));
-
-          r->from = tmp[j][k];
-          r->to = tmp[j][k-1];
-
-          tmp[j][k]->adj.push_back(r);
-        }
-      }
-    
-      // avenues ascend from south to north
-      if (tmp[j][k]->avenue == highest || tmp[j][k]->avenue % 5 == 0) {
-        // highest avenue always goes in both direction
-        if (j < tmp.size() - 1) {
-          // add avenues going south to north (incrementing)
-          r->type = AVENUE;
-          r->number = k;
-          r->distance = sqrt(pow(tmp[j+1][k]->x - tmp[j][k]->x, 2) + pow(tmp[j+1][k]->y - tmp[j][k]->y, 2));
-          r->from = tmp[j][k];
-          r->to = tmp[j+1][k];
-         
-          tmp[j][k]->adj.push_back(r);
-        }
-        if (j > 0) {
-          // add avenues going north to south (decrementing)
-          r->type = AVENUE;
-          r->number = k;
-          r->distance = sqrt(pow(tmp[j-1][k]->x - tmp[j][k]->x, 2) + pow(tmp[j-1][k]->y - tmp[j][k]->y, 2));
-
-          r->from = tmp[j][k];
-          r->to = tmp[j-1][k];
-          tmp[j][k]->adj.push_back(r);
-        }
-      }
-      else if (tmp[j][k]->avenue % 2 == 0) { // avenue is even
-        // even avenues are one way to the north (incrementing)
-        if (j < tmp.size() - 1) {
-          r->type = AVENUE;
-          r->number = k;
-          r->distance = sqrt(pow(tmp[j+1][k]->x - tmp[j][k]->x, 2) + pow(tmp[j+1][k]->y - tmp[j][k]->y, 2));
-
-          r->from = tmp[j][k];
-          r->to = tmp[j+1][k];
-
-          tmp[j][k]->adj.push_back(r);
-        }
-      }
-      else {
-        // odd avenues are one way to the south (decrementing)
-        if (j > 0) {
-          // odd avenues going north to south
-          r->type = AVENUE;
-          r->number = k;
-          r->distance = sqrt(pow(tmp[j-1][k]->x - tmp[j][k]->x, 2) + pow(tmp[j-1][k]->y - tmp[j][k]->y, 2));
-
-          r->from = tmp[j][k];
-          r->to = tmp[j-1][k];
-          tmp[j][k]->adj.push_back(r);
-        } 
-      } 
-
-
     }
+    bfsq.erase(bfsq.begin());
   }
 
-  
-}
-
-double City_Map::Dijkstra(int avg_best_worst) {
-  // perform Dijkstras algorithm to find the shortest path between the two points
-  (void) avg_best_worst;
-  // find the path from light at [0, 0] and the light with the largest street and avenue
+  // chase the back link
+  while (tmp != first){
+    path.push_front(tmp->backedge);
+    d +=tmp ->backedge->distance;
+    tmp = tmp->backedge->from;
+  }
   return 0;
 }
+
+City_Map::City_Map(){
+  string l;
+  int street, avenue;
+  double x, y, gstreet, gavenue;
+  int strt = 0;
+  int ave = 0;
+  list <Intersection *>::iterator lit;
+  list <Intersection *>::iterator sit;
+
+  // reading all Intersections
+  while (cin >> street >> avenue >> x >> y >> gstreet >> gavenue){
+    // create a new intersection
+    Intersection* I = new Intersection();
+    I->street = street;
+    I->avenue = avenue;
+    I->x = x;
+    I->y = y;
+    I->green[0] = gstreet;
+    I->green[1] = gavenue;
+    
+    I->best_time = 0; // for Djikstras
+    I->backedge = NULL;
+    I->bfsq_ptr = bfsq.end();
+
+    if (avenue > ave) ave = avenue; // find the highest street and avenure
+    if (street > strt) strt = street;
+    
+    all.push_back(I);
+  }
+
+  for (lit = all.begin(); lit != all.end(); lit++){
+    // highest street and avenue, set the last node
+    if (strt == (*lit)->street && ave == (*lit)->avenue) last = *lit;
+    // same with first node
+    if (0 == (*lit)->street && 0 == (*lit)->avenue) first = *lit;
+
+    for (sit = all.begin(); sit != all.end(); sit++){
+      // must check if the nodes are the same
+      if (*sit == *lit) continue;
+      Road_Segment* r = new Road_Segment(); // new segment
+      r->number = -1; // use the segment to check the placement of the nodes relative to eachother
+      // odd streets and multiples of 5
+      if ((*lit)->street % 2 != 0 || ((*lit)->street % 5 == 0 )){
+        if ((*lit)->street == (*sit)->street && (*lit)->avenue == (*sit)->avenue+1){
+          r->type = STREET;
+          r->number = (*lit)->street;
+        }
+      }
+      // even numbers and multiples of 5
+      if((*lit)->street % 2 == 0 || ( (*lit)->street % 5 == 0)){
+        if((*lit)->street == (*sit)->street && (*lit)->avenue == (*sit)->avenue - 1){
+          r->type = STREET;
+          r->number = (*lit)->street;
+        }
+      }
+      // odd numbers and the highest avenue
+      if( (*lit)->avenue % 2 != 0 || (*lit)->avenue == ave || (*lit)->avenue % 5 == 0){
+        if ((*lit)->avenue == (*sit)->avenue && (*lit)->street == (*sit)->street + 1 ){
+          r->type = AVENUE;
+          r->number = (*lit)->avenue;
+        }
+      }
+      // even numbers and highest avenue
+      if( (*lit)->avenue % 2 == 0 || (*lit)->avenue% 5 == 0 || (*lit)->avenue == ave){
+        if ((*lit)->avenue == (*sit)->avenue && (*lit)->street == (*sit)->street - 1 ){
+          r->type = AVENUE;
+          r->number = (*lit)->avenue;
+        }
+      }
+      // set distance and pointers
+      if (r->number != -1){
+        r->from = *lit;
+        r->to = *sit;
+        r->distance = sqrt(pow((*lit)->x- (*sit)->x,2) +
+                           pow((*lit)->y - (*sit)->y,2));
+        (*lit)->adj.push_back(r);
+      }
+
+      else {
+        delete r;
+      }
+    }
+  }
+}
+
