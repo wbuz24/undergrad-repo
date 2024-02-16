8dcb305ba0b7391133ba04d18768fcf964e65d6f
wbuz24 <wbuziak@vols.utk.edu>
2024-02-16 16:57:22 -0500

    48/100

--- S24/cs360/labs/Lab-3-Huffman/src/huff_dec.c
+++ S24/cs360/labs/Lab-3-Huffman/src/huff_dec.c
@@ -7,8 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* recursive free, call on children then free node */
-
 typedef struct huff_node {
   struct huff_node *Ptrs[2];
   char *Strings[2];
@@ -16,23 +14,97 @@ typedef struct huff_node {
 
 void freeNodes(Node *n) {
 
+  /* recursive free, call on children then free node */
   if (n->Ptrs[0] != NULL) freeNodes(n->Ptrs[0]);
   if (n->Ptrs[1] != NULL) freeNodes(n->Ptrs[1]);
 
-  if (n->Strings[0] != NULL) printf("%s\n", n->Strings[0]);
-  if (n->Strings[1] != NULL) printf("%s\n", n->Strings[1]);
+  /* Free strings */
+  if (n->Strings[0] != NULL) {
+    free(n->Strings[0]);
+  }
+
+  if (n->Strings[1] != NULL) {
+    free(n->Strings[1]);
+  }
+
+  /* Free nodes */
   free(n);
 
   return;
 }
 
+/* recursive printing for testing */
+void print_tree(Node *root) {
+  if (root->Ptrs[0] != NULL) print_tree(root->Ptrs[0]);
+  if (root->Ptrs[1] != NULL) print_tree(root->Ptrs[1]);
+
+  if (root->Strings[0] != NULL) printf("%s\n", root->Strings[0]);
+  if (root->Strings[1] != NULL) printf("%s\n", root->Strings[1]);
+}
+
+void insert_into_tree(Node *root, char *bitstring, char *decoding) {
+  Node *prev, *p;
+    
+  /* If this is the last bit, insert the word */
+  if (strlen(bitstring) == 1) {
+    if (*bitstring == '0') root->Strings[0] = strdup(decoding);
+    if (*bitstring == '1') root->Strings[1] = strdup(decoding);
+  }
+
+  else {
+    /* Bit is zero */
+    if (*bitstring == '0') {
+    /* Chase the pointer */
+      if (root->Ptrs[0] != NULL) insert_into_tree(root->Ptrs[0], bitstring+1, decoding);
+    /* Create the Node and recursive call */
+      else {
+        prev = root;
+        p = (Node *) malloc(sizeof(Node));
+        p->Ptrs[0] = NULL;
+        p->Ptrs[1] = NULL;
+        p->Strings[0] = NULL;
+        p->Strings[1] = NULL;
+        prev->Ptrs[0] = p;
+        insert_into_tree(p, bitstring+1, decoding); 
+      }
+    }
+    /* Bit is one */
+    if (*bitstring == '1') {
+      if (root->Ptrs[1] != NULL) insert_into_tree(root->Ptrs[1], bitstring+1, decoding); 
+      else {
+        prev = root;
+        p = (Node *) malloc(sizeof(Node));
+        p->Ptrs[0] = NULL;
+        p->Ptrs[1] = NULL;
+        p->Strings[0] = NULL;
+        p->Strings[1] = NULL;     
+        prev->Ptrs[1] = p;
+        insert_into_tree(p, bitstring+1, decoding);
+      }
+    }
+  }
+}
+
+/* Grab the next bit and return it */
+int get_bit(char *buff, int n) {
+  int nth, ret;
+
+  nth = n / 8;
+  ret = (buff[nth] >> (n % 8)) & 1;
+
+  if (ret > 0) return 1;
+  else return 0;
+}
+
+
 int main(int argc, char **argv) {
   FILE *def, *input; // file pointer
-  char c, h;
-  char word[1000];
-  int l;
+  char c;
+  char *d;
+  char word[1000], nums[1000];
+  int i, l;
   u_int32_t last, bytes;
-  Node *n, *p, *prev;
+  Node *n, *p;
 
   /* Check args */
   if (argc != 3) {
@@ -50,13 +122,21 @@ int main(int argc, char **argv) {
   } 
 
   fseek(input, -4, SEEK_END);
-  last = fgetc(input);
-  bytes = (last / 8) + 4;
+  l = ftell(input); // total
+  
+/*  if (l < 4) {
+    fprintf(stderr, "Error: file is not the correct size.\n");
+    return 0;
+  }*/
+  fread(&last, 1, 4, input);
   fseek(input, 0, SEEK_SET);
 
-  // printf("%u\n\n", bytes);
+  bytes = last / 8 + 4; // # of bytes
+  
+//  printf("%d %u\n\n", l * 8, last);
 
-  if (bytes >= 4) {
+  if (l > (int) last) fprintf(stderr, "Error: file is not the correct size.\n");
+  else {
     
     /* Create the bottom node in the tree, n */
     n = (Node *) malloc(sizeof(Node));
@@ -65,10 +145,8 @@ int main(int argc, char **argv) {
     n->Strings[0] = NULL;
     n->Strings[1] = NULL;
 
-
-  while (fread(&c, 1, 1, def) > 0) {
+    while (fread(&c, 1, 1, def) > 0) {
       /* p is the node being processed */
-      p = n;
       l = 0;
       if (c != '\0' && c != '0' && c != '1') {
         /* Insert the string into word */
@@ -82,40 +160,52 @@ int main(int argc, char **argv) {
 
       /* Character is 0 or 1 */
       if (c == '0' || c == '1') {
-        /* Chase nodes until you find the null character */
+        /* Store the number into a buffer */
+        l = 0;
         while (c != '\0') {
-          h = c; // grab the last character
-          if (c == '0') {
-            /* If the zero pointer is set, chase it, otherwise make a new node */
-            if (p->Ptrs[0] != NULL) p = p->Ptrs[0];
-            if (p->Strings[0] == NULL) {
-              prev = p;
-              p = (Node *) malloc(sizeof(Node));
-              p->Ptrs[0] = prev;
-              p->Ptrs[1] = NULL;
-            }
-          }
-          if (c == '1') {
-            if (p->Ptrs[1] != NULL) p = p->Ptrs[1];
-            if (p->Strings[1] == NULL) {
-              prev = p;
-              p = (Node *) malloc(sizeof(Node));
-              p->Ptrs[0] = NULL;
-              p->Ptrs[1] = prev;
-            }
-          }
+          nums[l] = c;
+          l++;
           fread(&c, 1, 1, def);
-        }
-        /* Check the value that got you here and copy the word */
-        if (h == '0') p->Strings[0] = strdup(word);
-        if (h == '1') p->Strings[1] = strdup(word);
-      } 
+        } 
+        nums[l] = '\0';
+        /* Insert into a tree */
+        insert_into_tree(n, nums, word);
+      }
     }
 
-    /* do a DFS, freeing the nodes from the bottom up */
-    freeNodes(n);
-  }
+    //print_tree(n);
+    /* Read in bytes from the input file */
 
+    d = (char *) malloc(sizeof(char) * bytes + 1);
+    fread(d, 1, bytes, input);
+    p = n;
+
+    for (i = 0; i < (int) last; i++) {
+      l = get_bit(d, i);
+      //printf("%d", l);
+      if (!l && p->Ptrs[0] != NULL) p = p->Ptrs[0];
+      else if (!l) {
+        printf("%s", p->Strings[0]);
+        p = n;
+      }
+
+      if (l && p->Ptrs[1] != NULL) p = p->Ptrs[1];
+      else if (l) {
+        printf("%s", p->Strings[1]);
+        p = n;
+      }      
+    }
+
+//    printf("\n\n");
+
+    // print_tree(n);
+
+    /* do a DFS, freeing the nodes from the bottom up */
+    free(d);
+    freeNodes(n);
+    fclose(def);
+    fclose(input);
+  }
 
   return 1;
 }
