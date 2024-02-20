4c0e4d84e9f90407fb23904096fff385962536ff
wbuz24 <wbuziak@vols.utk.edu>
2024-02-20 15:41:36 -0500

    fakemake local

--- S24/cs360/labs/Lab-4-Fakemake/fakemake.c
+++ S24/cs360/labs/Lab-4-Fakemake/fakemake.c
@@ -13,7 +13,7 @@
 int main(int argc, char **argv) {
   IS is;
   FILE *def, *output;
-  Dllist clist, tmp;
+  Dllist clist, hlist, tmp, htmp;
   char *filename;
   int i;
 
@@ -32,28 +32,43 @@ int main(int argc, char **argv) {
   }
 
   /* Open the output makefile */
-  output = fopen("makefile", "w");
+  //  output = fopen("makefile", "w");
 
   /* Input struct declaration */
   is = new_inputstruct(filename);
 
   clist = new_dllist();
+  hlist = new_dllist();
   while(get_line(is) >= 0) {
     if (is->NF <= 0) {
-//      printf("blank line \n");
+      //      printf("blank line \n");
     }
-    /* Grab C lines */
-    if (strcmp(is->fields[0], "C") == 0) {
-      printf("%s\n", is->text1);
-      for (i = 1; i < is->NF; i++) dll_append(clist, new_jval_s(is->fields[i]));
+    else {
+      /* Grab C lines */
+      if (strcmp(is->fields[0], "C") == 0) {
+        for (i = 1; i < is->NF; i++) {
+          printf("%s\n", is->fields[1]);
+          dll_append(clist, new_jval_s(strdup(is->fields[i])));
+        }
+      }
+      /* Grab H lines */
+      if (strcmp(is->fields[0], "H") == 0) {
+        for (i = 1; i < is->NF; i++) {
+          printf("%s\n", is->fields[1]);
+          dll_append(hlist, new_jval_s(strdup(is->fields[i])));
+        }
+      }
     }
   } 
 
   /* Traverse the dllist */
-  dll_traverse(tmp, clist) printf("%s", tmp->val.s);
+  dll_traverse(tmp, clist) printf("%s ", tmp->val.s);
+  printf("\n");
+  dll_traverse(htmp, hlist) printf("%s ", htmp->val.s);
+  printf("\n");
 
   jettison_inputstruct(is);
-  fclose(output);
+  //  fclose(output);
   fclose(def);
 
 }
