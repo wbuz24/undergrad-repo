9ea955c7a3df7a4a2b27217212130189efc68b20
wbuz24 <wbuziak@vols.utk.edu>
2024-03-04 01:02:15 -0500

    jgr unfinished

--- S24/cs494/labs/Lab-1-Jgraph/src/motd.cpp
+++ S24/cs494/labs/Lab-1-Jgraph/src/motd.cpp
@@ -49,26 +49,68 @@ string color_picker() {
 /* Construct the jgr file */
 void Jgraph::make_jgraph() {
 	ofstream ofile;
-	string color, s;
-  map <string, int>::iterator nit;
+	string color, s, mstr;
+	int maxd;
+  map <string, int>::iterator nit, mit;
 	char buf[1000];
 
-	ofile.open("Music-Of-The-Day.jgr");
+	ofile.open("jgr/Music-Of-The-Day.jgr");
 
 	ofile << "newgraph\n\n";
-	ofile << "xaxis\n  min 1000 max 2020\n  label : Decade\n";
-	ofile << "yaxis\n  min 0 max 12\n  label : Number of Occurances\n";
+	ofile << "xaxis\n  min 1805 max 2020\n  mhash 0\n  hash 10\n shash 0\n  size 6\n  label : Decade\n";
+	ofile << "yaxis\n  min 0 max 40\n  grid_lines grid_gray .7\n  size 3.5\n  mhash 2\n  hash 5\n  label : Number of Occurances\n";
 
+	/* Create a bar graph of decades */
+	maxd = 0;
+	ofile << "newline pts 1805 0 2020 0\n";
+	ofile << "newcurve marktype xbar cfill color 0 0 1";	
+	ofile << "\n  marksize 8.5\n\n";
+	ofile << "\n         pts";
 	for (nit = Decades.begin(); nit != Decades.end(); nit++) {
-		color = color_picker();
-		sprintf(buf, "label : %s", nit->first.c_str());
-    s = buf;
-	  ofile << "newcurve " << color << s;	
-		ofile << "\n         pts ";
-		sprintf(buf, "%s %d\n", nit->first.c_str(), nit->second);
+		sprintf(buf, " %s %d", nit->first.c_str(), nit->second);
+		if (nit->second > maxd) {
+			maxd = nit->second;
+			mstr = nit->first;
+		}
 		s = buf;
 		ofile << s;
 	}
+	ofile << endl;
+
+	ofile << "newstring hjl vjt x 1975 y 29\n";
+	ofile << "          font Times-Italic fontsize 20 : " << mstr;
+	ofile << endl;
+
+  /* Plot the highest decade */
+	ofile << "newcurve marktype xbar cfill color 1 0 0";
+	ofile << "\n  marksize 8.5\n\n";
+	ofile << "\n         pts";
+	sprintf(buf, " %s %d\n", mstr.c_str(), maxd); 
+	ofile << buf << endl;
+
+	ofile << "newcurve marktype circle linetype solid label : Individual Years";
+	ofile << "\n         pts";
+
+	/* Plot the individual years */
+	maxd = 0;
+	for (mit = Dates.begin(); mit != Dates.end(); mit++) {
+		sprintf(buf, " %s %d", mit->first.c_str(), mit->second);
+		if (mit->second > maxd) {
+			maxd = mit->second;
+			mstr = mit->first;
+		}
+		s = buf;
+		ofile << s;
+	}
+	ofile << endl;
+
+
+
+
+	/* Legend defaults */
+
+	ofile << "legend defaults x 1970 y 45 font Times-Italic fontsize 18\n\n\n\n";
+	ofile.close();
 }
 
 /* Dr. Plank, if youre reading this I am sorry ab the funct name it's just the way it is */
@@ -86,6 +128,7 @@ bool Jgraph::oldManMusic(string date) {
 			if (date[3] == '5') return 0;
 		} 
 	}
+	if (strcmp(date.c_str(), "1000") == 0 || strcmp(date.c_str(), "1800") == 0) return 0;
 
 	/* Construct the decade */
   decade = "";
@@ -104,11 +147,24 @@ bool Jgraph::oldManMusic(string date) {
 	return 1;
 }
 
+/* remove xxxx's & xxxx. */
+bool isFiveorSix(string word) {
+  if (word.length() == 5) {
+		if (word[4] == '.' || word[4] == ':' || word[4] == ',' || word[4] == ')') return 1;
+	}
+	if (word.length() == 6) {
+		if (word[4] == '\'' && word[5] == 's') return 1;	
+	}
+	return 0;
+}
+
 /* make sure the string is a four digit number */
 bool isFourDigits(string word) {
 	int i, d;
 	char c[1];
+	/* Check that the length is four */
 	if (word.length() != 4) return 0;
+	/* Make sure each character is a digit */
 	for (i = 0; i < 4; i++) {
 		c[0] = word[i];
 		if (sscanf(c, "%d", &d) != 1) return 0;
@@ -118,18 +174,25 @@ bool isFourDigits(string word) {
 }
 
 bool Jgraph::dateSearch(string word) {
+	int i;
 	string yr;
 
 	/* I am counting multiple performances */
 	/* For classical performances, I count the year of the performance */
 
+  /* check for 's and periods */
+	if (isFiveorSix(word)) {
+		/* just grab the first four characters */
+		yr = "";
+		for (i = 0; i < 4; i++) yr += word[i];
+		word = yr;
+	}
+
 	if (isFourDigits(word)) {
 			/* I assume Dr. Plank only listens to music from year 1000 to 2014 */
-			if (oldManMusic(word)) {
-				return 1;
-			}
+			if (oldManMusic(word)) { return 1; }
 	}
-  
+
 	return 0;
 }
 
@@ -168,6 +231,19 @@ int main(int argc, char** argv) {
 		while (getline(fin, line)) {
 			jgraph->splitLine(line);
 		}
+
+    /* Franz Liszt */
+		line = "1853";
+		jgraph->oldManMusic(line);
+    /* Johannes Brahms */
+		line = "1867";
+		jgraph->oldManMusic(line);
+    /* Ludwig von Beethoven 7th symphony */
+		line = "1811";
+		jgraph->oldManMusic(line);
+
+
+
 
 		jgraph->make_jgraph();
 
