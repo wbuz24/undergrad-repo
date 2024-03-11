22020d34c1ff83e5664378e915a8ff438c3b6b0c
wbuz24 <wbuziak@vols.utk.edu>
2024-03-11 17:32:35 -0400

    10/100 tarc

--- S24/cs360/labs/Lab-5-Tarc/src/tarc.c
+++ S24/cs360/labs/Lab-5-Tarc/src/tarc.c
@@ -15,7 +15,7 @@
 /* Comparison function for JRB tree */
 int compare(Jval v1, Jval v2) {
   if (v1.l < v2.l) return -1;
-  if (v1.l < v2.l) return 1;
+  if (v1.l > v2.l) return 1;
   return 0;
 }
 
@@ -25,91 +25,106 @@ char* grab_path(char *absolute) {
   /* Find the index of the last / in the absolute
    * path and return a pointer to it */
   len = strlen(absolute);
-  for (i = len - 1; i >= 0; i--) {
+  for (i = len - 2; i >= 0; i--) {
     if (absolute[i] == '/') return absolute + i + 1; 
   } 
 
   return absolute;
 }
 
-void directory_traverse(char *pathname, JRB inodes) {
-  DIR *d;
-  struct dirent *de;
-  struct stat buf;
+void directory_traverse(char *fn, JRB inodes, char *relative) {
+  DIR *d;              
+  struct dirent *de;     
+  struct stat buf;          
   FILE *file;
-  int exists, fn_size, dir_fn_size, sz, suff_size;
-  int fname, fmode;
-  long in, mtime, filesize;
-  char *dir_fn, *bytes, *suffix, *relative;
 
-  /* Attempt to open the directory */
-  d = opendir(pathname);
-  if (d == NULL) { perror(pathname); exit(1); }
+  char *dir_fn, *bytes, *rel_fn; 
+  int dir_fn_size, sz, fn_size, suff_size, exists, rel_size, fname;
 
-  /* Construct the entire pathname */
-  fn_size = strlen(pathname);
+  Dllist directories, tmp;  /* Dllist of directory names, for doing recusion after closing. */
+
+  /* Initialize */
+  d = opendir(fn);
+  if (d == NULL) {
+    perror(fn);
+    exit(1);
+  }
+  directories = new_dllist();
+
+  /* Start building the directory + files.   We'll start by setting dir_fn_size to fn_size+10,
+     and we'll make it bigger as we need to.  It will be more efficient to use a number bigger
+     than 10 for this, but 10 will let us debug the code if there's a problem. 
+
+     I'm also setting up dir_fn to hold the directory name and a slash. */
+
+  fn_size = strlen(fn);
   dir_fn_size = fn_size + 10;
   dir_fn = (char *) malloc(sizeof(char) * dir_fn_size);
-  if (dir_fn == NULL) { perror(dir_fn); exit(1); }
-  strcpy(dir_fn, pathname);
+  if (dir_fn == NULL) { perror("malloc dir_fn"); exit(1); }
+  strcpy(dir_fn, fn);
   strcat(dir_fn + fn_size, "/");
 
-  /* Just a pointer to the suffix of the absolute path */
-  suffix = grab_path(dir_fn);
-  relative = (char *) malloc(sizeof(char) * dir_fn_size + 10);
-  suff_size = strlen(suffix);
-  strcpy(relative, suffix);
-  strcat(relative + suff_size, "/");
+  suff_size = strlen(relative);
+  rel_size = suff_size + 10;
+  rel_fn = (char *) malloc(sizeof(char) * rel_size);
+  if (rel_fn == NULL) { perror("malloc rel_fn"); exit(1); }
+  strcpy(rel_fn, relative);
+  strcat(rel_fn + suff_size, "/");
 
-  /* Run through the entire directory */
+  /* Run through the directory and run stat() on each file, 
+     keeping track of the total size of all of the files.  */
   for (de = readdir(d); de != NULL; de = readdir(d)) {
 
-    /* Reallocate for the size of the filename */
-    sz = strlen(de->d_name);
-    if (dir_fn_size < fn_size + sz + 2) {
-      dir_fn_size = fn_size + sz + 10;
-      dir_fn = realloc(dir_fn, dir_fn_size);
-    }
+    /* First, we need to build dir_fn.  First check to see if it's big enough, and if not,
+       we'll call realloc() to reallocate space.  Then we put the filename after the
+       slash. */
 
-    /* Add the suffix after the / */
-    strcpy(dir_fn + fn_size + 1, de->d_name);
-
-    /* Check the existence of each file */
-    exists = stat(dir_fn, &buf);
-    if (exists < 0) { fprintf(stderr, "Could'nt open %s\n", dir_fn); continue; }
-    /* We don't care about . and .. */
     if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+      sz = strlen(de->d_name);
+      if (dir_fn_size < fn_size + sz + 2) {    /* The +2 is for the slash and null character. */
+        dir_fn_size = fn_size + sz + 10;
+        dir_fn = realloc(dir_fn, dir_fn_size);
+      }
+      strcpy(dir_fn + fn_size + 1, de->d_name);
 
+      if (rel_size < suff_size + sz + 2) {
+        rel_size = suff_size + sz + 10;
+        rel_fn = realloc(rel_fn, rel_size);
+      }
+      strcpy(rel_fn + suff_size + 1, de->d_name);
+
+      exists = lstat(dir_fn, &buf);
+      if (exists < 0) {
+        fprintf(stderr, "Couldn't stat %s\n", dir_fn);
+        exit(1);
+      } 
       /* Print the size of the file's name, as a four-byte integer in little endian */
-      fname = strlen(dir_fn);
+      fname = strlen(rel_fn);
       fwrite(&fname, 4, 1, stdout);
-      /* Print the file's name, no null character */
-      fwrite(dir_fn, 1, fname,  stdout); 
+
+      /* Print the file's name, no null character */ 
+      fwrite(rel_fn, 1, fname,  stdout); 
+
       /* Print the file's inode as an eight byte long in little endian */
       fwrite(&buf.st_ino, 8, 1, stdout);
 
       /* If you haven't seen the inode before */
       if (jrb_find_gen(inodes, new_jval_l(buf.st_ino), compare) == NULL) {
-        /* Add it */
         jrb_insert_gen(inodes, new_jval_l(buf.st_ino), new_jval_i(0), compare);
 
-        /* Print the file's mode as a four byte integer in little endian */
+        /* Print the file's mode as a four byte integer in little endian */ 
         fwrite(&buf.st_mode, 4, 1, stdout);
-        /* Print the file's lst modification time, in seconds, as an eight byte long in little endian */
+        /* Print the file's last modification time, in seconds, as an eight byte long in little endian */
         fwrite(&buf.st_mtime, 8, 1, stdout);
       }
-
-      /* Attempt to open the sub directory */
+      /* Don't make the recursive call, but instead put the directory into the dllist. */
       if (S_ISDIR(buf.st_mode)) {
-        /* Construct the relative path */
-        strcat(relative + suff_size + 1, de->d_name);
-        printf("\n\n\n%s   %s\n\n\n", relative, dir_fn);
-        directory_traverse(relative, inodes);
+        dll_append(directories, new_jval_s(strdup(dir_fn)));
       }
       else {
-        /* Print the file's size as an eight byte long in little endian */
-        fwrite(&buf.st_size, 8, 1, stdout);
 
+        /* Print the file's size as an eight byte long in little endian */ 
+        fwrite(&buf.st_size, 8, 1, stdout);
         /* Open the file and error check */
         file = fopen(dir_fn, "r");
         if (file == NULL) { perror(dir_fn); exit(1); }
@@ -117,20 +132,35 @@ void directory_traverse(char *pathname, JRB inodes) {
         fread(bytes, 1, buf.st_size, file);
         fwrite(bytes, 1, buf.st_size, stdout); 
 
-        free(bytes);
         fclose(file);
+        free(bytes);
       }
+
     }
   }
 
-  /* Free the memory */
+  /* Make the recursive calls after you've closed the directory.  */
+
   closedir(d);
-  free(relative);
+
+  dll_traverse(tmp, directories) {
+    /* Reset the null character and concatenate strings */
+    directory_traverse(tmp->val.s, inodes, tmp->val.s);
+  }
+
+  /* Clean up.  You need to free the strings inside the dllist, because you
+     allocated them with strdup(), and they'll be a memory leak otherwise. */
+
+  dll_traverse(tmp, directories) free(tmp->val.s);
+  free_dllist(directories);
   free(dir_fn);
+  free(rel_fn);
 }
 
 int main(int argc, char** argv) {
-  char *pathname;
+  char *pathname, *relative;
+  struct stat buf;
+  int psize, exists;
   JRB inodes;
 
   if (argc != 2) { fprintf(stderr, "./bin/tarc directory/\n"); exit(1); }
@@ -138,9 +168,30 @@ int main(int argc, char** argv) {
   /* Grab the pathname */
   pathname = strdup(argv[1]); 
 
+  exists = lstat(pathname, &buf);
+  if (exists < 0) {
+    fprintf(stderr, "Couldn't stat %s\n", pathname);
+    exit(1);
+  } 
+
+
+  relative = grab_path(pathname);
+
   inodes = make_jrb();
+
+  /* Print the root directory */
+  psize = strlen(relative);
+
+  fwrite(&psize, 4, 1, stdout);
+  fwrite(relative, 1, psize, stdout);
+  fwrite(&buf.st_ino, 8, 1, stdout);
+  fwrite(&buf.st_mode, 4, 1, stdout);
+  fwrite(&buf.st_mtime, 8, 1, stdout);
+
+  jrb_insert_gen(inodes, new_jval_l(buf.st_ino), new_jval_i(0), compare);
+
   /* Recursively search the directory for files and subdirectories */
-  directory_traverse(pathname, inodes);
+  directory_traverse(pathname, inodes, relative);
 
   free(pathname);
   jrb_free_tree(inodes);
