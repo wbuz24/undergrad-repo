a47f13f88c06e55518dfafc17da9e724524732ad
wbuz24 <wbuziak@vols.utk.edu>
2024-03-20 18:23:09 -0400

    many gradescripts for import

--- S24/cs494/labs/Lab-2-FAT/FATRW.c
+++ S24/cs494/labs/Lab-2-FAT/FATRW.c
@@ -10,24 +10,51 @@
 #include <string.h>
 #include "jdisk.h"
 
-/* Read a link from the jdisk */
+/* Read a link from the jdisk and return the dereferenced bytes */
 unsigned short read_link(struct Disk *jd, unsigned short **p, int link) {
-  int sector, index;
+  int index, sector;
 
-  /* Calculate the indices of the lba */
-  sector = link / (JDISK_SECTOR_SIZE / 2);
-  index = link % (JDISK_SECTOR_SIZE / 2);
+  /* Calculate the indices of into the FAT */ 
+  sector = link / 512;
+  index = link % 512;
 
-  /* Only malloc when you need that sector */
+  /* Only allocate and read a sector when necessary */
   if (p[sector] == NULL) {
     p[sector] = (unsigned short *) malloc(sizeof(unsigned short) * (JDISK_SECTOR_SIZE / 2));
     if (p[sector] == NULL) { fprintf(stderr, "malloc link\n"); exit(1); }
-    jdisk_read(jd, link, p[sector]);
+    jdisk_read(jd, sector, p[sector]);
   }
 
-//  printf("%hu\n", p[sector][index]);
-  
   return p[sector][index];
+}
+
+/* Write a link to the FAT table, later this will be written to the disk */
+int write_link(unsigned short **p, int link, unsigned short val) {
+  int sector, index;
+  unsigned short nextlink;
+
+  sector = link / 512;
+  index = link % 512; 
+  /* Set the value at the current link to 0 in the FAT table */
+
+  p[sector][index] = 0;
+
+  /* Set the first link in the FAT to the sector after this one */
+  p[0][0] = val;  
+  
+  /* Track the sector you are making changes */
+  return sector;
+}
+
+/* Write the FAT sectors back to the jdisk */
+void flush_links(struct Disk *jd, unsigned short **p, int sector) {
+  /* You only need to call jdisk_write on two FAT sectors */
+
+  if (sector != 0) {
+    jdisk_write(jd, sector, p[sector]);
+  }
+
+  jdisk_write(jd, 0, p[0]);
 }
 
 int main(int argc, char **argv) {
@@ -35,11 +62,11 @@ int main(int argc, char **argv) {
   struct stat buf;
   struct Disk *jd;
   char *dfile, *ifile;
-  unsigned short **p;
-  int starting_block, exists;
+  unsigned short **p, *bytes;
+  int starting_block, exists, last1, last2;
   double lpb, links;
-  int s, d, n, i; 
-  short unsigned int lba, currlink, arrindex;
+  int s, d, n, i, x, sector, flag, newsec; 
+  short unsigned int lba, currlink, nextlink, fin, pen;
   long filesize, flsize;
 
   /* N - # of sectors is filesize / 1024
@@ -63,28 +90,30 @@ int main(int argc, char **argv) {
 
   n = jdisk_size(jd) / JDISK_SECTOR_SIZE; /* Number of blocks */
   lpb = JDISK_SECTOR_SIZE / 2; /* -> 1024/2 # of links per block */
-  d = (lpb * n - 1) / 513; /* Largest possible value that D+S is less than <= disksize*/
+  d = (lpb * n - 1) / 513; /* Data sectors */
   links = d + 1; /* The FAT is comprised of D+1 entries */
-  s = ceil(links / lpb); /* Smallest number that can hold D+1 shorts */
-  printf("Total Sectors: %d. Data Sectors: %d. FAT Sectors: %d.\n", n, d, s);
+  s = ceil(links / lpb); /* FAT sectors */
+  // printf("Total Sectors: %d. Data Sectors: %d. FAT Sectors: %d.\n", n, d, s);
 
   /* Allocate the buffer and an array of pointers that will be my FAT table */
   p = (unsigned short **) malloc(sizeof(unsigned short *) * s);
   if (p == NULL) { fprintf(stderr, "malloc FAT buffer\n"); exit(1); }
 
+  bytes = (unsigned short *) malloc(sizeof(unsigned short) * 512);
+  if (bytes == NULL) { fprintf(stderr, "malloc fileread buffer\n"); exit(1); }
+
   /* Allocate and Store the FAT table */
-  for (i = 1; i < s; i++) p[i] = NULL;
+  for (i = 0; i < s; i++) p[i] = NULL;
 
   /* You only need to start with the first sector */
   p[0] = (unsigned short *) malloc(sizeof(unsigned short) * lpb);
   if (p[0] == NULL) { fprintf(stderr, "malloc FAT buffer\n"); exit(1); }
   jdisk_read(jd, 0, p[0]);
 
-  currlink = p[0][0];  
-  lba = s + currlink - 1;
-  printf("New file starts at sector: %d\n", lba);
+  currlink = read_link(jd, p, 0); /* Start of the free list */  
+  newsec = s + currlink - 1;
 
-  /* Store the input file on a jdisk */
+  /* Import a file on a jdisk */
   if (argc == 4) {
 
     /* Check the filesize of the input file and whether the disk has enough free blocks to store the file */
@@ -99,21 +128,48 @@ int main(int argc, char **argv) {
     filesize = buf.st_size;
 
     /* Determine the number of free blocks */
-
     flsize = 0;
-    while (currlink != 0 && currlink != lba - s + 1) {
-      lba = s + currlink - 1; 
+    while (currlink != 0) {
+      nextlink = read_link(jd, p, currlink);
+      /* Only run so long as you need to */
       flsize += 1024;
+      if (flsize >= filesize || currlink == nextlink) break;
 
-      currlink = read_link(jd, p, lba);
+      currlink = nextlink;
     }
 
-    /* Calculate the amount of bytes in the last free block */
+    flsize += 1024;
 
-    /* If the file is larger than free blocks, throw an error */
-    if (flsize < filesize) { fprintf(stderr, "Not enough free blocks\n"); exit(1); }
+    if (flsize < filesize) { fprintf(stderr, "Not enough free blocks (%d) for %s, which needs %d\n", ((filesize - flsize) / 1024) + 1, ifile, (filesize / 1024) + 1); exit(1); }
 
+    /* Write blocks back to the jdisk */
 
+    flag = 0;
+    currlink = read_link(jd, p, 0); // FAT index 
+    while (filesize != 0) {
+      lba = currlink + s - 1; // index of the disk you will read
+
+      /* Read file's bytes into a buffer */
+      fread(bytes, 1, (filesize > 1024) ? 1024 : filesize, iofile);
+
+      /* Write the buffer's bytes onto the jdisk */
+      jdisk_write(jd, lba, bytes);
+
+      /* Final block of the file, reset the fat and return the sector that changed */
+      if (filesize <= 1024) {
+        sector = write_link(p, currlink, read_link(jd, p, currlink));
+      }
+
+      /* Update the size - if the size is greater that 1024, subtract, otherwise set it to zero */
+      filesize = (filesize > 1024) ? filesize - 1024 : 0; 
+      currlink = read_link(jd, p, currlink);
+    }
+
+    /* Actually call jdisk_write */
+
+    flush_links(jd, p, sector);
+
+    printf("New file starts at sector: %d\n", newsec);
     printf("Reads: %ld\nWrites: %ld\n", jdisk_reads(jd), jdisk_writes(jd));
 
     fclose(iofile);
@@ -121,6 +177,24 @@ int main(int argc, char **argv) {
   }
   if (argc == 5) {
     /* Find the file at the starting block and write to the output file */
+
+
+    /* Last block in the file may not be 1024 bytes */
+      /* Calculate the amount of bytes in the last free block */
+        /* Build the bytes in the sector with the last two bytes */
+    /*
+    if (currlink != 0) {
+      if (p[currlink][1023] == 0xff) flsize += 1023;
+      else {
+        last1 = (currlink * 1024) - 1;
+        last2 = last1 - 1;
+
+        fin = read_link(jd, p, last1);
+        pen = read_link(jd, p, last2);
+        flsize += fin * 256 + pen;
+//        printf("%hu %hu", pen, fin);
+      }
+    }*/
   
   }
 
